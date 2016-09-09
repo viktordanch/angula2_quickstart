f0bbac8b3fe6115a361b2a7d27cff175b29d0258
viktordanch <viktor.danch@gmail.com>
2016-09-09 06:08:23 +0300

    create reuseable component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
+
 
 @Component({
   selector: 'my-app',
@@ -12,14 +15,7 @@ import { Component } from '@angular/core';
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name">
-      </div>
-    </div>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
   `,
 
   styles: [`
@@ -81,11 +77,6 @@ export class AppComponent {
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-}
-
-export class Hero {
-  id: number;
-  name: string;
 }
 
 const HEROES: Hero[] = [
